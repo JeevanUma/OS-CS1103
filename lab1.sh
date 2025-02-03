be5140612d2e9c252a073ef294b03f04716f31f6
JeevanUma <jeevanacharya687@gmail.com>
2025-02-03 15:02:01 +0530

    Delete lab1.sh

--- lab1.sh
+++
@@ -1,9 +0,0 @@
-echo "parent process ID:$$"
-creat_child_process () {
-      echo "starting Child Process..."
-      sleep 3 
-      echo "Child Process ID: $PPID , Parent Process ID: $$"
-} 
-   create_child_process &
-    create_child_process & 
-    echo "All child processes have completed, Back to Parent Process ID: $$"
